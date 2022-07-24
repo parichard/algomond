2654c702c4c2d64a8c4af91eef980946ae82b716
Antonin POLY <apoly.kerneis@gmail.com>
2022-07-24 11:13:52 +0200

    first push

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import Rellax from 'rellax'
+
+createApp(App).mount('#app')
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 70;
+
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  const relax = new Rellax('.relax')
+
+  window.addEventListener("scroll", reveal);
