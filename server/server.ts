7e4d12c478e905cac17093bcd4cd7ad0a844322d
Antonin POLY <apoly.kerneis@gmail.com>
2022-09-11 20:30:42 +0200

    Final fix, hopefully ....

--- server/server.ts
+++ server/server.ts
@@ -19,7 +19,7 @@ import auth from './src/auth/authAPI'
 
 
     //use the serve-static package to serve the bundled app files in the dist directory
-    app.use('/', serveStatic(path.join(__dirname, '/dist')))
+    app.use('/', express.static(path.join(__dirname, '/dist')));
 
     app.use(json())
     app.use('/api/cards', cards) // defines url in browser
