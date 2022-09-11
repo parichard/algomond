cc870b2c319562f1d5ccac5133e373031c2ed08b
Antonin POLY <apoly.kerneis@gmail.com>
2022-09-11 19:04:22 +0200

    fix build

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import path from 'path'
+import serveStatic from 'serve-static'
 import { json } from 'body-parser'
 import { connectMongoDB, populateDB } from './core/db'
 import cors from 'cors'
@@ -8,26 +10,37 @@ import auth from './auth/authAPI'
 
 (async () => {
 
-  const PORT_NUMBER = 3000
+    const PORT_NUMBER = process.env.PORT || 3000
 
-  await connectMongoDB()
-  await populateDB()
+    await connectMongoDB()
+    await populateDB()
 
-  const app = express()
+    const app = express()
 
-  app.use(json())
-  app.use('/cards', cards) // defines url in browser
-  app.use('/', auth)
-  app.use(cors({origin: function() {return true}}))
-  app.use(function(error, req, res, next) {
-    const code = error.status || 500
-    res.status(code)
-    res.json({
-        status: error.status,
-        message: code === 500 ? 'There was an error try again later' : error.message,
+
+    //use the serve-static package to serve the bundled app files in the dist directory
+    app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+    // this * route is to serve project on different page routes except root `/`
+    app.get(/.*/, function (req, res) {
+
+        res.sendFile(path.join(__dirname, '/dist/index.html'))
+
     })
-})
-app.listen(PORT_NUMBER)
-console.log("server started on port " + PORT_NUMBER)
+
+    app.use(json())
+    app.use('/cards', cards) // defines url in browser
+    app.use('/', auth)
+    app.use(cors({origin: function() {return true}}))
+    app.use(function(error, req, res, next) {
+        const code = error.status || 500
+        res.status(code)
+        res.json({
+            status: error.status,
+            message: code === 500 ? 'There was an error try again later' : error.message,
+        })
+    })
+    app.listen(PORT_NUMBER)
+    console.log("server started on port " + PORT_NUMBER)
 
 })()
